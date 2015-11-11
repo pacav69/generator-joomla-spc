ab43226f0c424f45325f5da6209352f2b2f205ea
pacav69 <pacav69@gmail.com>
2015-11-12 05:36:05 +0800

    changed zip module to grunt-contrib-compress

--- app/templates/_Gruntfile.js
+++ app/templates/_Gruntfile.js
@@ -3,23 +3,35 @@ module.exports = function(grunt) {
 
     // Project configuration.
     grunt.initConfig({
-       pkg: grunt.file.readJSON('package.json'),
-        zip: {
-            'using-cwd': {
+        pkg: grunt.file.readJSON('package.json'),
+        compress: {
+            main: {
+                options: {
+                    mode: 'zip',
+                    archive: 'dist/' + 'com_default-value_v1.0.0.zip'
+                },
+                expand: true,
                 cwd: 'src/',
-                // Files will zip with src as the root
-                src: ['src/**'],
-                dest: 'dist/com_<%= _.slugify(componentName) %>_v<%= versionno %>.zip'
+                src: ['**/*'],
+                dest: ''
             }
         }
+        // zip: {
+        //     'using-cwd': {
+        //         cwd: 'src/',
+        //         // Files will zip with src as the root
+        //         src: ['src/**'],
+        //         dest: 'dist/com_default-value_v1.0.0.zip'
+        //     }
+        // }
 
     });
 
     // Load the plugin that provides the task.
 
-    grunt.loadNpmTasks('grunt-zip');
+    grunt.loadNpmTasks('grunt-contrib-compress');
 
     // Default task(s).
-    grunt.registerTask('default', ['zip']);
+    grunt.registerTask('default', ['compress']);
 
 };
